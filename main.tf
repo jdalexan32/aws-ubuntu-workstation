815a7209881c7d941c4b6f0c71295089a7004fd0
Jay Alexander <jdalexan32@gmail.com>
2024-05-06 20:15:03 +0000

    removed alt_rdp_source_ip and sg-rdp2

--- main.tf
+++ main.tf
@@ -24,10 +24,6 @@ variable "ssh_key" {
   description = "The AWS Key Pair to use for SSH"
 }
 
-variable "alt_rdp_source_ip" {
-  description = "An additional IP address from which to RDP"
-}
-
 data "http" "myip" {
   url = "https://api.ipify.org"
 }
@@ -119,15 +115,6 @@ resource "aws_security_group_rule" "sg-rdp" {
   to_port           = 3389
   protocol          = "tcp"
   cidr_blocks       = ["${chomp(data.http.myip.response_body)}/32"]
-  security_group_id = aws_security_group.work-sg.id
-}
-
-resource "aws_security_group_rule" "sg-rdp2" {
-  type              = "ingress"
-  from_port         = 3389
-  to_port           = 3389
-  protocol          = "tcp"
-  cidr_blocks       = [var.alt_rdp_source_ip]
   security_group_id = aws_security_group.work-sg.id
 }
 
